e60fff9b44036de7906d148bbc05a40d3dbfc4d5
FxWolfxe <ndmuench@comcast.net>
2020-05-22 13:59:52 -0400

    initial commit

---
+++ Source/PMTribal/PMTribal/PMTribalInit.cs
@@ -0,0 +1,37 @@
+﻿// ModInit.cs created by Iron Wolf for PMTribal on 05/22/2020 1:53 PM
+// last updated 05/22/2020  1:53 PM
+
+using System;
+using HarmonyLib;
+using Verse;
+
+namespace PMTribal
+{
+    [StaticConstructorOnStartup]
+    public static class PMTribalInit
+    {
+        public static string HARMONY_ID = "ironwolf.pawnmorpher.tribal";
+
+        static PMTribalInit()
+        {
+            var har = new Harmony(HARMONY_ID);
+
+            try
+            {
+                har.PatchAll();
+            }
+            catch (Exception e)
+            {
+                Log.Error($"caught {e.GetType().Name} while patching {HARMONY_ID}!\n{e}");
+            }
+        }
+    }
+
+
+    public class PMTribalMod : Mod
+    {
+        public PMTribalMod(ModContentPack content) : base(content)
+        {
+        }
+    }
+}
